f6cdd8a76e74578396269ecb0d722f90e363a358
Daniel Manz <manzdani@hu-berlin.de>
2018-04-27 13:05:36 +0200

    made RHF cryscor compatible

--- neu/RHF.f90
+++ neu/RHF.f90
@@ -1,52 +1,30 @@
-MODULE RHF
+MODULE manz_RHF
   implicit NONE
   private
-  public init,terminate,P_init,G_matrix,Energy,RHF_control,F_matrix
-  public P,G,F,vecs
-  public E,E_old,iters,vals
-  DOUBLE PRECISION, Dimension(:,:),allocatable :: P,G,F,vecs
-  DOUBLE PRECISION, Dimension(:),allocatable:: vals
-      DOUBLE PRECISION,public,allocatable,DImension(:,:,:,:):: twomatrix
-      DOUBLE PRECISION,public,allocatable,Dimension(:,:):: H
-      DOUBLE PRECISION,public::E_nucc
-      INTEGER:: iters
-      double precision:: E,E_old
-      INTEGER,PUBLIC::NORB,NELEC
+  public init,P_init,G_matrix,Energy,RHF_control,manz_RHF_controlinit
+      DOUBLE PRECISION,allocatable,DImension(:,:,:,:):: twomatrix
+      DOUBLE PRECISION,allocatable,DImension(:,:):: H
+      Double Precision::E_Nucc
+      INTEGER::NORB,NELEC,iunit_dump,MS2,ISYM
+      Double Precision,Dimension(100)::ORBSYM
+      CHARACTER(len=50)::filename_out
+      INTEGER::addelec
 
   CONTAINS
     SUBROUTINE init
       IMPLICIT NONE
       Integer, PARAMETER:: MAX_size=100
-      INTEGER, PARAMETER:: LU=99
       INTEGER:: my,ny,la,si,n,index1,index2,ierror,elecadd
-      INTEGER:: MS2,ISYM
-      DOUBLE PRECISION,DIMENSION(MAX_size)::ORBSYM
       Character(len=24):: filename
       Character(len=128):: ierrormsg
       NAMELIST/FCI/NORB,NELEC,MS2,ORBSYM,ISYM
       
-      CALL get_command_argument(1,filename)
-      open (Unit=LU, File=filename,Status='old', iostat=ierror)
-      errorcheck: IF (ierror>0) THen
-        write(*,*) ierror
-        write(*,*) 'fileerror'
-      END if errorcheck
-
-      READ(UNIT=LU,NML=FCI)
-      Write (*,1001)
-      1001 Format (1x,'HOW MUCH ELECTRONS TO ADD?')
-      Read (*,1011) elecadd 
-      1011 Format (I3)
-      NELEC=NELEC+elecadd
+      rewind(Unit=iunit_dump)
+      READ(UNIT=iunit_dump,NML=FCI)
+      NELEC=NELEC+addelec
       write (*,*) 'new NELEC:',NELEC,norb
-      allocate(P(NORB,NORB))
-      allocate(G(NORB,NORB))
-      allocate(F(NORB,NORB))
-      allocate(vecs(NORB,NORB))
-      allocate(vals(NORB))
-      ALLOCATE (twomatrix(NORB,NORB,NORB,NORB),H(NORB,NORB))
-      call read_integral
     END SUBROUTINE init 
+
 
   Subroutine read_integral
       INTEGER:: NORB_store, NELEC_store, MS2, ISYM, ierror
@@ -58,12 +36,8 @@ MODULE RHF
       Character(len=24):: filename
       NAMELIST/FCI/NORB,NELEC,MS2,ORBSYM,ISYM
       CALL get_command_argument(1,filename)
-      NORB_store=NORB
-      NELEC_store=NELEC
-      !open (Unit=LU, File=filename,Status='old', iostat=ierror)
-      !READ(UNIT=LU,NML=FCI)
       DO
-       READ(LU,300,IOSTAT=ierror,IOMSG=ierrormsg) integral, my, ny, la, si
+       READ(iunit_dump,300,IOSTAT=ierror,IOMSG=ierrormsg) integral, my, ny, la, si
       300 Format (1x,E23.16,4I4)
         If (ierror /= 0) then
           EXit
@@ -84,27 +58,24 @@ MODULE RHF
           twomatrix(si,la,ny,my)=integral
         end if
       END DO
-    NORB=NORB_store
-    NELEC=NELEC_store
+      close(unit=iunit_dump)
     END Subroutine read_integral
 
-    subroutine terminate
-      INTEGER::deallo_status
-deallocate(twomatrix,H,P,G,F,vecs,vals,STAT=deallo_Status)
-      end subroutine terminate
-
-    Subroutine P_init()
+    Subroutine P_init(P)
       IMPLICIT NONE
       integer::i=1
+      DOUBLE Precision,Dimension(NORB,NORB),intent(out)::P
+      P=0
       DO i = 1,int(NELEC/2)
         P(i,i)=2
       END DO
     END Subroutine P_init
 
-  Subroutine P_matrix
+  Subroutine P_matrix(P,vecs)
     Implicit NONE
     integer::my,ny,a
-    !write(*,*) vecs
+    Double Precision,Dimension(NORB,NORB),intent(out)::P
+    Double Precision,Dimension(NORB,NORB),intent(in)::vecs
     DO my=1,NORB
       DO ny=1,NORB
         P(my,ny)=0
@@ -113,13 +84,13 @@ deallocate(twomatrix,H,P,G,F,vecs,vals,STAT=deallo_Status)
         End Do
       End Do
     End Do
-    !write(*,*) P
   End Subroutine P_matrix
 
-  Subroutine G_matrix
+  Subroutine G_matrix(G,P)
     IMPLICIT NONE
     integer::my,ny,la,si
-
+    DOUBLE Precision,Dimension(NORB,NORB),Intent(out)::G
+    Double Precision,Dimension(NORB,NORB), INtent(in)::P
     Do my=1,NORB
       DO ny=1,NORB
         G(my,ny)=0
@@ -132,13 +103,11 @@ deallocate(twomatrix,H,P,G,F,vecs,vals,STAT=deallo_Status)
     END DO
   END Subroutine G_matrix
 
-  Subroutine F_matrix
-    F=H+G
-  End Subroutine F_matrix
-
-  Subroutine Energy
+  Subroutine Energy(E,P,G)
     IMPLICIT NONE
     INTEGER:: my,ny,la,si
+    DOUBLE PRECISion, intent(out)::E
+    Double precision, dimension(NORB,NORB),intent(in)::P,G
 
     E=E_nucc
     Do my=1,NORB
@@ -149,29 +118,26 @@ deallocate(twomatrix,H,P,G,F,vecs,vals,STAT=deallo_Status)
   END Subroutine ENergy
 
   Subroutine matprint(title,A)
-    DOUBLE PRECISION,DIMENSION(NORB,NORB)::A
+    DOUBLE PRECISION,DIMENSION(NORB,NORB),Intent(in)::A
     INTEGER::i
     CHARACTER::title
     write(*,*)title
     Do i=1,NORB
       write (*,1500) A(i,1),A(i,2),A(i,3),A(i,4),A(i,5),A(i,6),A(i,7),A(i,8),A(i,9),A(i,10),A(i,11),A(i,12),A(i,13)
-      1500 Format (' ',13F12.8)
+      1500 Format (' ',13F16.8)
     End Do
   End Subroutine matprint
 
-  Subroutine fcimaker
+  Subroutine fcimaker(vecs)
     IMPLICIT NONE
     INTEGER::I,J,K,L,my,ny,la,si
     DOUBLE PRECISION:: integral
-    INTEGER,DIMENSION(NORB)::ORBSYM
-    INTEGER::ierror,fcidumpnew,MS2=0,ISYM=0
-    CHARACTER(len=10)::filename
+    INTEGER::ierror,fcidumpnew
     CHARACTER(len=124)::imsg
+    DOUBLE Precision,Dimension(NORB,NORB),intent(in)::vecs
     NAMELIST/FCI/NORB,NELEC,MS2,ORBSYM,ISYM
-    CALL get_command_argument(2,filename)
-    ORBSYM=1
     fcidumpnew=25
-    OPEN(UNIT=fcidumpnew, FILE=filename, STATUS='REPLACE', ACTION='WRITE', IOSTAT=ierror)
+    OPEN(UNIT=fcidumpnew, FILE=filename_out, STATUS='REPLACE', ACTION='WRITE', IOSTAT=ierror)
     WRITE(UNIT=fcidumpnew,NML=FCI)
     DO I=1,NORB
       Do J=1,I
@@ -207,41 +173,52 @@ deallocate(twomatrix,H,P,G,F,vecs,vals,STAT=deallo_Status)
     if (ierror/=0) then
       write (*,*) imsg
     end if
-    !300 Format (1x,E23.16,4I4)
     END DO
   END DO
   I=0
   J=0
   integral=E_nucc
   WRITE(fcidumpnew,300,IOSTAT=ierror,IOMSG=imsg) integral, I, J, K, L
-  !300 Format (1x,E23.16,4I4)
 END SUbroutine fcimaker
 
-Subroutine RHF_control
+Subroutine RHF_control(P,G,vecs,vals)
 
 IMPLICIT NONE
 integer::iters=1,i,j
 DOUBLE PRECISION::E_old=0
+DOUBLE Precision::E=0
 INTEGER::INFO
 DOUBLE PRECISION, Dimension(:),allocatable::work
 integer :: lwork,lwork_max,deallo_STATUS
 double precision, parameter :: thresh=1d-10
 CHARACTER(len=124)::ERRalloc
-
-call INIT
+Double precision,Dimension(:,:),allocatable,intent(inout)::P,G,vecs
+DOUBLE PRECISION,Dimension(:),allocatable,intent(inout)::vals
+Double Precision,Dimension(:,:),allocatable::F
+call init
 lwork_max=norb**2
 allocate(work(lwork_max))
 lwork=NORB**2
+allocate(P(NORB,NORB),G(NORB,NORB),F(NORB,NORB),vecs(NORB,NORB),vals(NORB),H(NORB,NORB),twomatrix(NORB,NORB,NORB,NORB))
+P=0
+G=0
+F=0
+vecs=0
+vals=0
+H=0
+twomatrix=0
+work=0
+call read_integral
 
 DO
   if (iters==1) then
-    call P_init
+    call P_init(P)
   else
-    call P_matrix
+    call P_matrix(P,vecs)
   end if
-  call G_matrix
-  call F_matrix
-  call Energy
+  call G_matrix(G,P)
+  F=H+G
+  call Energy(E,P,G)
   call DSYEV('V','L',NORB,F,NORB,vals,work,lwork,INFO)
 do i=1,norb
  do j=1,norb
@@ -249,7 +226,7 @@ do i=1,norb
  end do
 end do
   write(*,1100)E,iters
-  1100 Format (' ','ENERGY:',F10.4,'Iteration:',I3)
+  1100 Format (' ','ENERGY:',F20.14,'Iteration:',I3)
   if (abs(E_old-E)<(thresh)) then
     exit
   end if
@@ -260,16 +237,40 @@ end do
   end if
 END DO
 write(*,*) 'DONE Calculating'
-Call fcimaker
+Call fcimaker(vecs)
 write(*,*) "DONE FCIMAKER"
 deallocate(work,stat=deallo_Status,ERRMSG=ERRalloc)
-Call terminate
+deallocate(twomatrix)
+deallocate(H)
+deallocate(P)
+deallocate(G)
+deallocate(F)
+deallocate(vecs)
+deallocate(vals)
 write(*,*)'deallocated'
 end Subroutine RHF_control
 
-END MODULE RHF
+Subroutine manz_RHF_controlinit(iunit_dump_stor,filename_out_stor,addelec_stor)
+  Double Precision, Dimension(:,:),allocatable::P,G,vecs
+  Double Precision, Dimension(:),allocatable::vals
+  Integer::iunit_dump_stor,addelec_stor
+  CHARACTER(len=50)::filename_out_stor
+  iunit_dump=iunit_dump_stor
+  filename_out=filename_out_stor
+  addelec=addelec_stor
+  call RHF_control(P,G,vecs,vals)
+END Subroutine manz_RHF_controlinit
 
-PROGRAM main
-  use RHF
-  call RHF_control
-END Program
+END MODULE manz_RHF
+
+PROGRAM RHF_main
+  use manz_RHF
+  INTEGER::iunit_dump=99
+  Character (len=50)::filename_in,filename_out
+  INTEGER::addelec
+  addelec=0
+  CALL get_command_argument(1,filename_in)
+  Call get_command_argument(2,filename_out)
+  open (Unit=iunit_dump, File=filename_in,Status='old',action='read')
+  call manz_RHF_controlinit(iunit_dump,filename_out,addelec)
+END PROGRAM
