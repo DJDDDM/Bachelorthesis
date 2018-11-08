3ead46e3e1312d278ab275c77fe1d288c2e758d7
Daniel Manz <manzdani@hu-berlin.de>
2018-11-08 11:20:18 +0100

    commit before refactoring

--- dev/mybaby.jl
+++ dev/mybaby.jl
@@ -1,50 +1,69 @@
 #!/apps/JULIA/1.0.1/bin/julia
+module HF
 using Compat
-using TensorToolbox
 using TensorOperations
 using PyCall
 using Combinatorics
-using IterTools
 using LinearAlgebra
 @pyimport f90nml
 import Base.print_matrix
 
+mutable struct DIIS_container
+diis_error_matrices
+diis_fock_matrices
+diis_bmat
+diis_energy
+citers
+end
+
 function main()
-    two, H, E_nucc, NORB, NELEC = FCIread()
-    NELEC += parse(Int,ARGS[2])
-    println("E_nucc= ",E_nucc," NORB= ",NORB," NELEC= ",NELEC)
     global maxdiis
-    maxdiis = 5
+
+    global doRHF
+    global doUHF
+    global doCUHF
+    global doMP2
+
     global NORB
     global NELEC
     global NOCC
     global NVIRT
     global E_nucc
-    NOCC = convert(Int,NELEC/2)
-    NVIRT = NORB-NOCC
 
-    E_RHF,vals, C = RHF(two,H,E_nucc,NORB,NOCC)
-    println("finished RHF E = $(E_RHF)")
+    doRHF = 1
+    doUHF = 0
+    doCUHF = 1
+    doMP2 = 0
+    maxdiis = 5
 
-    #E_UHF, Ca, Cb, valsa, valsb = UHF(H,two)
-    #println("finished UHF", "E = ", E_UHF)
+    two, H, E_nucc, NORB, NELEC = FCIread()
+    NELEC += parse(Int,ARGS[2])
+    println("E_nucc= ",E_nucc," NORB= ",NORB," NELEC= ",NELEC)
 
+    if doRHF == 1
+        NOCC = convert(Int,NELEC/2)
+        NVIRT = NORB-NOCC
+        E_RHF,vals, C = RHF(two,H,E_nucc,NORB,NOCC)
+        println("finished RHF E = $(E_RHF)")
+    end
 
-    #E_CUHF, Ca, Cb, valsa, valsb = CUHF(H,two)
-    #println("finished CUHF", "E = ", E_CUHF)
+    if doUHF == 1
+        E_UHF, Ca, Cb, valsa, valsb = UHF(H,two)
+        println("finished UHF", "E = ", E_UHF)
+    end
 
-    #E_UHF, Ca, Cb, valsa, valsb = CUHF(H,two)
-    #println("finished CUHF", "E = ", E_UHF)
-
-    #3twoMO = transformtwo(C,two)
-    #3HMO = transformone(C,H)
-    #3println("transform")
-    #3@time transformtwo(C,two)
-
-    #3E_MP2 = MP2(twoMO,vals,NOCC,NORB)
-    #3println("E_MP2=",E_MP2)
-    #3println("E_RHF+E_MP2= ",E_RHF + E_MP2)
-    #3@time MP2(two,vals,NOCC,NORB)
+    if doCUHF == 1
+        E_CUHF, Ca, Cb, valsa, valsb = CUHF(H,two)
+        println("finished CUHF", "E = ", E_CUHF)
+    end
+    
+    #show(stdout,"text/plain",C)
+    if doMP2 == 1
+        twoMO = C' * C' * twoAO * C * C
+        E_MP2 = MP2(twoMO,vals,NOCC,NORB)
+        println("E_MP2=",E_MP2)
+        println("E_RHF+E_MP2= ",E_RHF + E_MP2)
+    end
 
 end
 
@@ -106,7 +125,7 @@ end
 function RHF(two,H,E_nucc,NORB,NOCC)
     E = 0.0
     E_old = 0.0
-    C = eye(NORB)
+    C = Matrix{Float64}(I,NORB,NORB)
     iters = 1
     vals = zeros((NORB))
 
@@ -185,11 +204,14 @@ function UHF(H,two)
 
     E = 0.0
     E_old = 0.0
-    Ca = eye(NORB)
-    Cb = eye(NORB)
+    Ca = Matrix{Float64}(I,NORB,NORB)
+    Cb = Matrix{Float64}(I,NORB,NORB)
     iters = 1
     valsa = zeros((NORB))
     valsb = zeros((NORB))
+
+    DIISa = DIIS_init()
+    DIISb = DIIS_init()
 
     if NELEC%2 == 0
         NELECa = convert(Int,(NELEC/2))
@@ -206,9 +228,6 @@ function UHF(H,two)
         Pa = Ca[:,1:NELECa]*Ca[:,1:NELECa]'
         Pb = Cb[:,1:NELECb]*Cb[:,1:NELECb]'
         Pt = Pa + Pb
-        #for i = 1:NORB
-        #    println(Pt[i,:])
-        #end
 
         #P -> G
         Ga = tensorcontract(Pa,["la","si"], two, ["my","la","si","ny"])
@@ -223,6 +242,9 @@ function UHF(H,two)
         E = E_nucc + 0.5 * (tensorcontract(Pt, ["ny","my"], H, ["my", "ny"])
                       + tensorcontract(Pa, ["ny","my"], Fa, ["my", "ny"])
                       + tensorcontract(Pb, ["ny","my"], Fb, ["my", "ny"]))
+
+        #DIIS
+        Fa,Fb,diis_error = DIIS(DIISa,(Fa,Fb),(Pa,Pb),E,iters)
 
         #F->C
         eiga = LinearAlgebra.eigen(Fa)
@@ -242,7 +264,7 @@ function UHF(H,two)
 
         #loopcontrol
         println("iters= ",iters,"Energy= ", E)
-        if abs(E-E_old)<10.0^-7
+        if diis_error < 10^-6 
             break
         end
         if iters > 1000
@@ -258,11 +280,12 @@ function UHF(H,two)
 end
 
 function CUHF(H,two)
+    DIIS_container = DIIS_init()
 
     E = 0.0
     E_old = 0.0
-    Da = eye(NORB)
-    Db = eye(NORB)
+    Da = Matrix{Float64}(I,NORB,NORB)
+    Db = Matrix{Float64}(I,NORB,NORB)
     iters = 1
     valsa = zeros((NORB))
     valsb = zeros((NORB))
@@ -283,30 +306,24 @@ function CUHF(H,two)
         Pb_AO = Db[:,1:NELECb]*Db[:,1:NELECb]'
         Pt_AO = (Pa_AO + Pb_AO)/2
         Pd_AO = (Pa_AO - Pb_AO)/2
-        #println("Pt_AO")
-        #show(stdout,"text/plain",Pt_AO)
-        #println("")
-        #println("Pa_AO")
-        #show(stdout,"text/plain",Pt_AO)
-        #println("")
-        #println("Pb_AO")
-        #show(stdout,"text/plain",Pt_AO)
-        #println("")
 
         #NO
         eig = LinearAlgebra.eigen(Pt_AO)
         U = eig.vectors
         vals = real.(eig.values)
-        println(vals)
         idx = sortperm(vals,rev=true)
         U = U[:,idx]
 
         #F_AO
         Fcs_AO = H + 2*tensorcontract(two,["i","j","k","l"],Pt_AO,["l","k"]) - tensorcontract(two,["i","l","k","j",],Pt_AO,["l","k"])
+        #DIIS
+        Fcs_AO,diis_error = DIIS(DIIS_container,Fcs_AO,Pt_AO,E,iters)
+
         Duhf_AO = tensorcontract(two,["i","l","k","j"],Pd_AO,["k","l"])
 
         Fa_AO = Fcs_AO - Duhf_AO
         Fb_AO = Fcs_AO + Duhf_AO
+
         
         #F_NO
         Fa_NO = U' * Fa_AO * U
@@ -358,18 +375,8 @@ function CUHF(H,two)
         end
         E_old = E
         iters +=1
-        println("spincontamtination")
-        println(tr(U'*Pa_AO*U*U'*Pb_AO*U))
     end
     return E, Da, Db, valsa, valsb
-end
-
-mutable struct DIIS_container
-diis_error_matrices
-diis_fock_matrices
-diis_bmat
-diis_energy
-citers
 end
 
 function DIIS_init()
@@ -382,34 +389,116 @@ container = DIIS_container(diis_error_matrices, diis_fock_matrices, diis_bmat, d
 return container
 end
 
+function EDIIS(container,bsize)
+    #rhs
+    rhs = zeros(bsize+1)
+    rhs[1:bsize] = container.diis_energy[1:bsize]
+    rhs[bsize+1] = 1
+
+    #lhs
+    lhs = zeros(bsize+1,bsize+1)
+    lhs[bsize+1,:] .= 1
+    lhs[:,bsize+1] .= 1
+    lhs[bsize+1,bsize+1] = 0
+    lhs[1:bsize,1:bsize] = container.diis_bmat[1:bsize,1:bsize]
+
+    #solve
+    while true
+        X = lhs \ rhs
+        #remove
+        if minimum(X[1:end-1]) < 0
+            i = argmin(X[1:end-1])
+            lhs = lhs[setdiff(1:end,i),setdiff(1:end,i)]
+            rhs = rhs[setdiff(1:end,i)]
+            if length(rhs) == 2
+                return [1,0]
+                break
+            end
+            continue
+        else
+            return X
+            break
+        end
+    end
+end
+
+function CDIIS(container)
+    container.citers += 1
+    csize = min(maxdiis,container.citers)
+
+    rhs = zeros(csize+1)
+    rhs[csize+1] = -1
+
+    lhs = zeros(csize+1,csize+1)
+    lhs[csize+1,:] .= -1
+    lhs[:,csize+1] .= -1
+    lhs[csize+1,csize+1] = 0
+    lhs[1:csize,1:csize] = container.diis_bmat[1:csize,1:csize]
+
+    X = lhs \ rhs
+    return X
+end
 
 function DIIS(container,F,P,energy,iters)
 
     bsize = min(iters,maxdiis)
 
-    #copy lower
-    for k = min(iters,maxdiis):-1:2
-        container.diis_error_matrices[k,:,:] = container.diis_error_matrices[k-1,:,:]
-        container.diis_fock_matrices[k,:,:] = container.diis_fock_matrices[k-1,:,:]
-        container.diis_energy[k] = container.diis_energy[k-1]
-    end
-    for i = bsize:-1:2, j = bsize:-1:2
-        container.diis_bmat[i,j] = container.diis_bmat[i-1,j-1]
+    if doRHF == 1 || doCUHF == 1
+        #copy lower
+        for k = min(iters,maxdiis):-1:2
+            container.diis_error_matrices[k,:,:] = container.diis_error_matrices[k-1,:,:]
+            container.diis_fock_matrices[k,:,:] = container.diis_fock_matrices[k-1,:,:]
+            container.diis_energy[k] = container.diis_energy[k-1]
+        end
+        for i = bsize:-1:2, j = bsize:-1:2
+            container.diis_bmat[i,j] = container.diis_bmat[i-1,j-1]
+        end
+
+        #errormatrix
+        error_mat = F*P
+        error_mat -= error_mat'
+
+        #store recent
+        container.diis_error_matrices[1,:,:] = error_mat
+        container.diis_fock_matrices[1,:,:] = F
+        container.diis_energy[1] = energy
+
+        #e-vectors
+        e_max = maximum(error_mat)
+        e_min = minimum(error_mat)
+        e_abs = max(abs(e_max),abs(e_min))
+
+    elseif doUHF == 1
+        #initialize Fock as tuple of Fa and Fb
+        if iters == 1
+            container.diis_fock_matrices = (zeros(maxdiis,NORB,NORB),zeros(maxdiis,NORB,NORB))
+        end
+        #copy lower
+        for k = min(iters,maxdiis):-1:2
+            container.diis_error_matrices[k,:,:] = container.diis_error_matrices[k-1,:,:]
+            container.diis_fock_matrices[1][k,:,:] = container.diis_fock_matrices[1][k-1,:,:]
+            container.diis_fock_matrices[2][k,:,:] = container.diis_fock_matrices[2][k-1,:,:]
+            container.diis_energy[k] = container.diis_energy[k-1]
+        end
+        for i = bsize:-1:2, j = bsize:-1:2
+            container.diis_bmat[i,j] = container.diis_bmat[i-1,j-1]
+        end
+
+        #errormatrix
+        error_mat = F[1]*P[1]+F[2]*P[2]
+        error_mat -= P[1]*F[1]+P[2]*F[2]
+
+        #store recent
+        container.diis_error_matrices[1,:,:] = error_mat
+        container.diis_fock_matrices[1][1,:,:] = F[1][:,:]
+        container.diis_fock_matrices[2][1,:,:] = F[2][:,:]
+        container.diis_energy[1] = energy
     end
 
-    #errormatrix
-    error_mat = F*P
-    error_mat -= error_mat'
-
-    #e-vectors
+    #e-abs
     e_max = maximum(error_mat)
     e_min = minimum(error_mat)
     e_abs = max(abs(e_max),abs(e_min))
-
-    #store recent
-    container.diis_error_matrices[1,:,:] = error_mat
-    container.diis_fock_matrices[1,:,:] = F
-    container.diis_energy[1] = energy
 
     #bmatrix
     for i = 1:bsize
@@ -421,76 +510,37 @@ function DIIS(container,F,P,energy,iters)
     #which DIIS?
     #NODIIS
     if e_abs < 10^-6
-        return F, e_abs
-
+        X = [1,0]
     #EDIIS
     elseif e_abs > 0.01
-        #rhs
-        rhs = zeros(bsize+1)
-        rhs[1:bsize] = container.diis_energy[1:bsize]
-        rhs[bsize+1] = 1
+        X = EDIIS(container,bsize)
+    #CDIIS
+    elseif  e_abs <= 0.01
+        X = CDIIS(container)
 
-        #lhs
-        lhs = zeros(bsize+1,bsize+1)
-        lhs[bsize+1,:] .= 1
-        lhs[:,bsize+1] .= 1
-        lhs[bsize+1,bsize+1] = 0
-        lhs[1:bsize,1:bsize] = container.diis_bmat[1:bsize,1:bsize]
-
-        #solve
-        while true
-            X = lhs \ rhs
-            #remove
-            if minimum(X[1:end-1]) < 0
-                i = argmin(X[1:end-1])
-                lhs = lhs[setdiff(1:end,i),setdiff(1:end,i)]
-                rhs = rhs[setdiff(1:end,i)]
-                if length(rhs) == 2
-                    return F,e_abs
-                    break
-                end
-                continue
-            end
-            break
-        end
-
-        #new fock
+    end
+    #new Fock
+    if doRHF == 1
         diis_fock = zeros(NORB,NORB)
         for i = 1:length(X)-1
             diis_fock += X[i]*container.diis_fock_matrices[i,:,:]
         end
-        return F, e_abs
-
-    #CDIIS
-    elseif  e_abs <= 0.01
-        container.citers += 1
-        csize = min(maxdiis,container.citers)
-
-        rhs = zeros(csize+1)
-        rhs[csize+1] = -1
-
-        lhs = zeros(csize+1,csize+1)
-        lhs[csize+1,:] .= -1
-        lhs[:,csize+1] .= -1
-        lhs[csize+1,csize+1] = 0
-        lhs[1:csize,1:csize] = container.diis_bmat[1:csize,1:csize]
-
-        X = lhs \ rhs
-
-        diis_fock = zeros(NORB,NORB)
-        for i = 1:csize
-            diis_fock += X[i]*container.diis_fock_matrices[i,:,:]
+    return diis_fock, e_abs
+    elseif doUHF == 1
+        diis_Fa= zeros(NORB,NORB)
+        diis_Fb= zeros(NORB,NORB)
+        for i = 1:length(X)-1
+            diis_Fa += X[i]*container.diis_fock_matrices[1][i,:,:]
+            diis_Fb += X[i]*container.diis_fock_matrices[2][i,:,:]
         end
-        return diis_fock, e_abs
+    return diis_Fa,diis_Fb, e_abs
     end
-    end
-
+end
+end
 
             
 
         
+import HF
+HF.main()
 
-
-
-
-main()
